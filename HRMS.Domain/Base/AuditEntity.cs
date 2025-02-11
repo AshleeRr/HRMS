8c198e5df5300d3212ff84422a9c8afe078cb975
CristXanderAza <cristopherxanderazadiaz19111@gmail.com>
2025-02-11 19:52:45 -0400

    Edición de AuditEntity

--- HRMS.Domain/Base/AuditEntity.cs
+++ HRMS.Domain/Base/AuditEntity.cs
@@ -4,8 +4,6 @@ namespace HRMS.Domain.Base
     public abstract class AuditEntity
     {
         public DateTime FechaCreacion { get; set; }
-        public DateTime? FechaModificacion { get; set; }
-        public int UsuarioModificacion { get; set; }
         public bool? Estado { get; set; } = true;
     }
 }
