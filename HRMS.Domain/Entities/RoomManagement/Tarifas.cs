758a3cfc2ea316d9f6cadab39b9c047e5875a9a1
Awilka Jerome <Awilka Jerome>
2025-02-11 20:38:42 -0400

    Tarifas - awi

---
+++ HRMS.Domain/Entities/RoomManagement/Tarifas.cs
@@ -0,0 +1,18 @@
+﻿using HRMS.Domain.Base;
+
+namespace HRMS.Domain.Entities.RoomManagement
+{
+    public class Tarifas : AuditEntity
+    {
+        public int IdTarifa { get; set; }
+        public int IdHabitacion { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public decimal PrecioPorNoche { get; set; }
+        public decimal Descuento { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+
+
+
+    }
+}
