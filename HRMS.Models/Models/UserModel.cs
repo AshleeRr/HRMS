e973bda694f59f6aa3facb468cb9166628d71da6
Ashlee <ashleeramirezrosario@gmail.com>
2025-02-11 22:10:50 -0400

    Se hicieron los modelos de Cliente, Users y UsersRol

---
+++ HRMS.Models/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HRMS.Models.Models
+{
+    public class UserModel : PersonModel
+    {
+        public int IdUsuario { get; set; }
+       
+        public string? Clave { get; set; }
+        public int IdUserRol { get; set; }
+
+    }
+}
