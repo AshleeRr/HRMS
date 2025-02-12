62f2a468a3d083eda581dd7e2ecc0e556b913dfe
Ashlee <ashleeramirezrosario@gmail.com>
2025-02-11 23:02:17 -0400

    Se agregaron metodos exclusivos de IUserRepo, IClientRepo, IRoleUserRepo

--- HRMS.Persistence/Interfaces/IRoleUserRepository.cs
+++ HRMS.Persistence/Interfaces/IRoleUserRepository.cs
@@ -1,9 +1,12 @@
-﻿using HRMS.Domain.Entities.Users;
+﻿using HRMS.Domain.Base;
+using HRMS.Domain.Entities.Users;
 using HRMS.Domain.Repository;
 
 namespace HRMS.Persistence.Interfaces
 {
     public interface IRoleUserRepository : IBaseRepository<UserRole, int>
     {
+        Task<UserRole> GetRoleByDescription(string description);
+        Task<OperationResult> UpdateDescription(int idRolUsuario, string descripcion);
     }
 }
--- HRMS.Persistence/Interfaces/IUserRepository.cs
+++ HRMS.Persistence/Interfaces/IUserRepository.cs
@@ -1,10 +1,15 @@
-﻿using HRMS.Domain.Entities.Users;
+﻿using HRMS.Domain.Base;
+using HRMS.Domain.Entities.Users;
 using HRMS.Domain.Repository;
+using System.Threading.Tasks;
 
 namespace HRMS.Persistence.Interfaces
 {
     public interface IUserRepository : IBaseRepository<Users, int>
     {
-        //metodos exlucisvos de la entidad
+        Task<Users> GetUserByUserId(int idUsuario);
+        Task<OperationResult> DeleteUserbyId(int idUsuario);
+        Task<Users> GetUserByName(string nombreCompleto);
+        Task<OperationResult> UpdatePassword(int idUsuario, string nuevaClave);
     }
 }
