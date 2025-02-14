4f0a235c521996973a88355db4438976f0d72be1
Ashlee <ashleeramirezrosario@gmail.com>
2025-02-13 22:10:12 -0400

    Arreglando problemas de users

--- HRMS.Persistence/Interfaces/IUserRoleRepository.cs
+++ HRMS.Persistence/Interfaces/IUserRoleRepository.cs
@@ -2,11 +2,10 @@
 using HRMS.Domain.Entities.Users;
 using HRMS.Domain.Repository;
 
-namespace HRMS.Persistence.Interfaces.Users
+namespace HRMS.Persistence.Interfaces
 {
     public interface IUserRoleRepository : IBaseRepository<UserRole, int>
     {
-        Task<UserRole> GetRoleByUserRolId(int idRolUsuario);
         Task<OperationResult> UpdateDescription(int idRolUsuario, string nuevaDescripcion);
     }
 }
