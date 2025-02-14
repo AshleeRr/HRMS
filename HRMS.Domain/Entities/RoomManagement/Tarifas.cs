2577b821d4dbeccc4f464bfb7514b4b3125b2749
Awilka Jerome <Awilka Jerome>
2025-02-13 22:49:03 -0400

    Cambios Tarifas

--- HRMS.Domain/Entities/RoomManagement/Tarifas.cs
+++ HRMS.Domain/Entities/RoomManagement/Tarifas.cs
@@ -1,18 +1,21 @@
-﻿using HRMS.Domain.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using HRMS.Domain.Base;
 
 namespace HRMS.Domain.Entities.RoomManagement
 {
+    [Table("Tarifas")]
     public class Tarifas : AuditEntity
     {
-        public int IdTarifa { get; set; }
-        public int IdHabitacion { get; set; }
+        [Key]
+        public int IdTarifa { get; set; } 
+
+        [ForeignKey("IdHabitacion")] 
+        public int? IdHabitacion { get; set; }
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
         public decimal PrecioPorNoche { get; set; }
         public decimal Descuento { get; set; }
         public string Descripcion { get; set; } = string.Empty;
-
-
-
     }
 }
