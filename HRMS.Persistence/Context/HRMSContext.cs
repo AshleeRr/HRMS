1ab4c19fb5073d8c13fd81ce80bf69a2cd75d4bd
CristXanderAza <cristopherxanderazadiaz19111@gmail.com>
2025-02-13 22:18:28 -0400

    .

--- HRMS.Persistence/Context/HRMSContext.cs
+++ HRMS.Persistence/Context/HRMSContext.cs
@@ -1,4 +1,6 @@
-﻿using HRMS.Domain.Entities.Users;
+﻿using HRMS.Domain.Entities.Reservation;
+using HRMS.Domain.Entities.RoomManagement;
+using HRMS.Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace HRMS.Persistence.Context
@@ -6,11 +8,19 @@ namespace HRMS.Persistence.Context
     public class HRMSContext : DbContext
     {
         public HRMSContext(DbContextOptions<HRMSContext> options) : base(options){}
-            // Todas las entidades
+
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<Users> Users { get; set; }
         public DbSet<Client> Clients { get; set; }
 
+        public DbSet<Reservation> Reservations { get; set; }
+
+        
+        public DbSet<Habitacion> Habitaciones { get; set; }
+        public DbSet<EstadoHabitacion> EstadoHabitaciones { get; set; }
+        public DbSet<Piso> Pisos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tarifas> Tarifas { get; set; }
 
     }
 }
