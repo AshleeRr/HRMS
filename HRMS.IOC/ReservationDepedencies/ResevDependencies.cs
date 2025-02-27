e04579828d4dcc9dabf6cf55b3380203e7400dbc
CristXanderAza <cristopherxanderazadiaz19111@gmail.com>
2025-02-26 20:12:20 -0400

    Validations Edited

---
+++ HRMS.IOC/ReservationDepedencies/ResevDependencies.cs
@@ -0,0 +1,15 @@
+﻿using HRMS.Domain.Repository;
+using HRMS.Persistence.Repositories.Reserv;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRMS.IOC.ReservationDepedencies
+{
+    public static class ResevDependencies
+    {
+        public static IServiceCollection AddReceptionDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IReservationRepository, ReservationRepository>();
+            return services;
+        }
+    }
+}
