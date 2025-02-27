f181fd26564d28b01e67899b62fd8d2fbd1af4e1
CristXanderAza <cristopherxanderazadiaz19111@gmail.com>
2025-02-26 21:06:33 -0400

    Modificaciones

--- HRMS.Domain/Entities/Users/Client.cs
+++ HRMS.Domain/Entities/Users/Client.cs
@@ -1,4 +1,5 @@
-﻿using HRMS.Domain.Base;
+﻿
+using HRMS.Domain.Base;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,10 +9,9 @@ namespace HRMS.Domain.Entities.Users
     public class Client : UserAuditEntity
     {
         [Key]
-        public int idCliente { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdCliente { get; set; }
         public string? TipoDocumento { get; set; }
         public string? Documento { get; set; }
-        [ForeignKey("IdUsuario")]
-        public int? IdUsuario { get; set; } // FK
     }
 }
