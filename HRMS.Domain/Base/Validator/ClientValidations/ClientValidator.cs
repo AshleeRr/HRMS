4ea83e55384141ff4d782cdc711af331d5560f44
Ashlee <ashleeramirezrosario@gmail.com>
2025-03-04 15:55:34 -0400

    Se agregaron mas validaciones a userRole y a client

--- HRMS.Domain/Base/Validator/ClientValidations/ClientValidator.cs
+++ HRMS.Domain/Base/Validator/ClientValidations/ClientValidator.cs
@@ -15,6 +15,8 @@ namespace HRMS.Domain.Base.Validator.ClientValidations
                 .WithErrorMessage("El número de documento debe de tener ENTRE 6 y 15 caracteres");
             AddRule(c => c.Correo != null && c.Correo.Length <= 50)
                 .WithErrorMessage("El correo debe de tener menos de 50 caracteres");
+            AddRule(c => c.IdUsuario != null && c.IdUsuario != 1)
+                .WithErrorMessage("El id de usuario no puede ser nulo y debe ser 1");
         }
     }
 }
--- HRMS.Domain/Base/Validator/UserRoleValidations/UserRoleValidator.cs
+++ HRMS.Domain/Base/Validator/UserRoleValidations/UserRoleValidator.cs
@@ -11,6 +11,8 @@ namespace HRMS.Domain.Base.Validator.UserRoleValidatons
                 .WithErrorMessage("El rol de usuario no puede ser nulo");
             AddRule(ur => ur.Descripcion != null && ur.Descripcion.Length <= 50)
                 .WithErrorMessage( "La descripcion rol de usuario debe tener menos de 50 caracteres");
+            AddRule(ur => ur.RolNombre != null && ur.RolNombre.Length <= 30)
+                .WithErrorMessage("El nombre del rol de usuario debe tener menos de 50 caracteres");
         }
     }
 }
