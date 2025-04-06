a784b9b611126267a95dd48a1e771873b114f1c2
Slifer1212 <Supremeyunior008@gmail.com>
2025-04-05 20:06:29 -0400

    Clase para menssaje de valiciones de la api

---
+++ WebApi/Models/ApiErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models
+{
+    public class ApiErrorResponse
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public string Detail { get; set; }
+    }
+}
